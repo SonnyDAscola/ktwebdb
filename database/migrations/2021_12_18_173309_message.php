6445632a2aba7ebe53ede6a9817184e1d50f6705
Lars Kühn <bitbucket@innerste.net>
2021-12-18 18:41:00 +0100

    migrate messages

---
+++ database/migrations/2021_12_18_173309_message.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Message extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('receiver_id')->constrained('users')->cascadeOnUpdate()->restrictOnDelete();
+            $table->foreignId('sender_id')->constrained('users')->cascadeOnUpdate()->restrictOnDelete();
+            $table->timestamp('date_send')->nullable(false);
+            $table->timestamp('date_read')->nullable(true);
+            $table->boolean('star_by_receiver')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
